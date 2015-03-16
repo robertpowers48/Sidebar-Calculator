463454a87c6f6039673df0de6239ad0749fe6de0
Giuseppe Attanasio <master94ga@gmail.com>
2015-03-16 11:31:34 +0100

    Fixed some problem

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 // Get all the keys from document
 var keys = document.querySelectorAll('#calculator span');
-var operators = ['+', '-', 'x', '÷'];
+var operators = ['+', '-', 'x', '/'];
 var decimalAdded = false;
 
 // Add onclick event to all the keys and perform operations
@@ -18,19 +18,13 @@ for(var i = 0; i < keys.length; i++) {
 			decimalAdded = false;
 		}
 
-		//Remove "/" if screen is empty
-		else if((btnVal == '/') && (document.getElementById('screen').innerHTML == ''))
-		{
-			document.getElementById('screen').innerHTML = '';
-		}
-		
 		// If eval key is pressed, calculate and display the result
 		else if(btnVal == '=') {
 			var equation = inputVal;
 			var lastChar = equation[equation.length - 1];
 			
-			// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
-			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+			// Replace all instances of x  with *. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+			equation = equation.replace(/x/g, '*');
 			
 			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
 			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
